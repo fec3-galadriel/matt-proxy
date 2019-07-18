8035ce8dbecda7b1d4c9c3ff2b7362460ec36bfd
matt-leyrat <matt.leyrat@gmail.com>
2019-07-18 11:00:53 -0700

    Add route for reviews, adjust route for related products

--- server/index.js
+++ server/index.js
@@ -8,18 +8,20 @@ const proxy = httpProxy.createProxyServer();
 app.use('/:id', express.static('./public'));
 
 app.all('/api/photos/:id', (req, res) => {
-  console.log('connection to photos successful');
   proxy.web(req, res, { target: 'http://localhost:3001'});
 });
 
 app.all('/api/itemSummary/:id', (req, res) => {
-  console.log('connection to itemSumamry succesful');
   proxy.web(req, res, { target: 'http://localhost:3002' });
 })
 
-app.all('/api/relatedProducts/:id', (req, res) => {
+app.all('/api/product/:pid', (req, res) => {
   proxy.web(req, res, { target: 'http://localhost:3003' });
 })
+
+app.all('/api/reviews/:id', (req, res) => {
+  proxy.web(req, res, { target: 'http://localhost:3004' })
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
