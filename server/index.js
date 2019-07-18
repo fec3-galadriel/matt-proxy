9650555376d33cf3cfc6dd5842012da653890f7e
matt-leyrat <matt.leyrat@gmail.com>
2019-07-17 21:16:41 -0700

    Remove comments

--- server/index.js
+++ server/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
-// const proxy = require('express-http-proxy');
 
 const app = express();
 const PORT = '3000';
 const proxy = httpProxy.createProxyServer();
 
-// app.use('/:id', express.static('public'));
 app.use('/:id', express.static('./public'));
-
-// app.use('/api/photos/', proxy('localhost:3001', { ProxyReqPathResolver : (req) => `/api/photos${req.url}` }));
-
-// app.use('/api/itemSummary/', proxy('localhost:3002', { ProxyReqPathResolver : (req) => `/api/itemSummary${req.url}` }));
 
 app.all('/api/photos/:id', (req, res) => {
   console.log('connection to photos successful');
